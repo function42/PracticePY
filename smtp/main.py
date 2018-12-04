9c1872a76893fdff601dfeac824c57753278c82f
function42 <function42@foxmail.com>
2018-12-05 02:08:57 +0800

    update

--- smtp/main.py
+++ smtp/main.py
@@ -15,12 +15,12 @@ password = 'xxxxxxxxxxxxxxxx'
 to_addr = 'yyyyyy@yyyyyy.com'
 smtp_server = 'zzzz.zzzz.com'
 
-msg = MIMEText('hello, this is a message from myself', 'plain', 'utf-8') #邮件正文
-msg['From'] = _format_addr('myself <%s>' % from_addr) #邮件头部，发送者信息
-msg['To'] = _format_addr('myself <%s>' % to_addr) #接收者信息
-msg['Subject'] = Header('备份成功', 'utf-8').encode() #邮件主题
+msg = MIMEText('hello, this is a message from myself', 'plain', 'utf-8') # 邮件正文
+msg['From'] = _format_addr('myself <%s>' % from_addr) # 邮件头部，发送者信息
+msg['To'] = _format_addr('myself <%s>' % to_addr) # 接收者信息
+msg['Subject'] = Header('备份成功', 'utf-8').encode() # 邮件主题
 server = smtplib.SMTP(smtp_server, 25) # SMTP协议默认端口是25
-server.set_debuglevel(1) #打印出和SMTP服务器交互的所有信息
-server.login(from_addr, password) #登录SMTP服务器
-server.sendmail(from_addr, [to_addr], msg.as_string()) #发送邮件
+server.set_debuglevel(1) # 打印出和SMTP服务器交互的所有信息
+server.login(from_addr, password) # 登录SMTP服务器
+server.sendmail(from_addr, [to_addr], msg.as_string()) # 发送邮件
 server.quit()
